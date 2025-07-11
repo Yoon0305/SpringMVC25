8c3255255738a81b1f860eba06b29e71451ba6f2
220-30\Yoon <kroate131@gmail.com>
2025-07-11 18:01:46 +0900

    프론트 연결 완료 (CRUD)

--- src/main/java/org/mbc/domain/BoardVO.java
+++ src/main/java/org/mbc/domain/BoardVO.java
@@ -4,18 +4,16 @@ import java.util.Date;
 
 import lombok.Data;
 
-@Data // 생성자, getter/setter, toString 등 담당
+@Data // import lombok.Data; (생성자, 게터/세터/ toString 등... )
 public class BoardVO {
-	
-	//객체용 VO는 읽기전용 속성이 강하게 만드는 객체(getter위주로 셋팅)
-	//dto 는 getter/setter 모두 가지고 활용하는 객체
+	// 객체용으로 vo는 읽기전용 속성이 강하게 만드는 객체(getter위주로 셋팅)
+	// dto는 게터/세터 모두 가지고 활용하는 객체
 	
 	private Long bno;
 	private String title;
 	private String content;
-	private String writer;
-	private Date regDate;
-	private Date updateDate;
-	
+	private String writer ;
+	private Date regDate ;  // import java.util.Date;
+	private Date updateDate ;
 	
 }
--- src/main/java/org/mbc/service/BoardService.java
+++ src/main/java/org/mbc/service/BoardService.java
@@ -5,27 +5,26 @@ import java.util.List;
 import org.mbc.domain.BoardVO;
 
 public interface BoardService {
-	// interface는 단독으로 사용할 수 없는 객체 (동작문이 없기 때문)
-	// 여기서 선언한 method는 무조건 abstract method로 사용된다.
-	// interface를 사용하려면 구현 Class 를만들고 실행문을 넣게 된다.
-	// interface를 사용하는 이유는 조장이 method명을 강제로 셋팅하기 위함이다.
+	// 인터페이스는 단독으로 사용할 수 없는 객체임
+	// 여기서 선언한 메서드는 무조건 추상메서드로 사용된다.( 실행문없이 끝난다. -> ; )
+	// 인터페이스를 사용하려면 구현 클래스를 만들고 실행문을 넣게 된다.
+	// 인터페이스를 사용하는 이유는 조장이 메서드명을 강제로 셋팅하기 위함이다.
 	
-	public void register(BoardVO boardVO); // 조장이 만든 method 명 (Signature)
-	// 게시판 등록용 method
-	
+	public void register(BoardVO board); // 조장이 만든 메서드명(시그니쳐)
+	// 게시판 등록용 메서드
 	
 	public BoardVO get(Long bno);
 	// 게시글 자세히 보기 (번호를 넣으면 객체가 나와야 한다.)
 	
 	public boolean modify(BoardVO board);
-	// 게시글 수정하기 (객체가 들어가서 수정이 되었는지 t/f)
+	// 게시글 수정하기 (객체가 들어가서 수정이 됐는지 t/f)
 	
 	public boolean remove(Long bno);
-	// 게시글 삭제하기 (번호가 들어가서 삭제가 되었는지 t/f)
+	// 게시글 삭제하기 (번호가 들어가서 삭제 됐는지 t/f)
 	
 	public List<BoardVO> getList();
-	// 이 메서드가 호출되면 List 타입으로 BoardVO 객체들이 넘어온다.
-	// List : 동적 List / <>  generic : 강제로 객체 타입을 정함.
+	// 이 메서드가 호출되면 리스트타입으로 BoardVO객체 들이 넘어온다.
+	// List : 동적리스트 / < > 제네릭 : 강제로 객체 타입을 정함.
 	
 	
 }
